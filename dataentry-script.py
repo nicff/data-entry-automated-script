72152c20794bac8c7f28f11ab717ac3e06f72cb3
Nicolas Cafferata <cafferata10@gmail.com>
2023-12-11 16:05:17 -0300

    Data entry script

--- dataentry-script.py
+++ dataentry-script.py
@@ -7,197 +7,192 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 
-def iniciar_navegador():
+def start_explorer():
     try:
         driver = webdriver.Chrome()
         return driver
     except WebDriverException:
-        input("Falta el ChromeDriver para ejecutar el programa. Apretá ENTER para instalarlo...")
-        print("Instalando ChromeDriver...")
+        input("ChromeDriver is not installed on your PC. Press 'ENTER' to install...")
+        print("Installing ChromeDriver...")
         return webdriver.Chrome(ChromeDriverManager().install())
 
-def seleccionar_opcion(id_menu, valor):
+def select_option(id_menu, value):
     try:
-        # Esperar hasta que el menú sea visible y clickeable
         WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, id_menu)))
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, id_menu)))
         menu = driver.find_element(By.ID, id_menu)
-
-        # Crear instancia de Select y seleccionar la opción
         select = Select(menu)
-        select.select_by_value(valor)
+        select.select_by_value(value)
 
     except Exception as e:
-        print(f"Error al seleccionar opción en menú '{id_menu}': {e}")
+        print(f"Error selecting option at menu '{id_menu}': {e}")
         input()
 
-def procesar_preguntas():
+def process_questions():
 
-    # Establecer un contador de preguntas
-    numero_pregunta = 0
+    question_number = 0
 
-    def cargar_pregunta(datos_pregunta):
+    def upload_question(question_data):
 
         try:
-            print(f"Ingresando datos de pregunta {numero_pregunta}")
+            print(f"Ingresando datos de pregunta {question_number}")
 
-            # Establecer el nivel de dificultad según el número de pregunta 
-            # Las preguntas 1-5 son Básico, 6-10 son Intermedio y las restantes son Avanzado
-            if numero_pregunta <= 5:
-                nivel = "1"
-            elif numero_pregunta <= 10:
-                nivel = "2"
+            # Difficulty level is set by question number and adapted to corresponding value in html menu
+            # Questions 1-5 are Básico, 5-10 are Intermedio and the rest Advanced
+            if question_number <= 5:
+                diff = "1"
+            elif question_number <= 10:
+                diff = "2"
             else:
-                nivel = "3"
+                diff = "3"
 
-            # Localizar el menú de dificultad desplegable por XPath, crear instancia de Select y seleccionar la opción correspondiente
+            # Click on difficulty level menu and enter corresponding value
             menu = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "(//select[@id='level_id'])[2]")))
             select = Select(menu)
-            select.select_by_value(nivel)
+            select.select_by_value(diff)
 
-            # Ingresar la pregunta
-            campo_pregunta = driver.find_elements(By.CLASS_NAME, "note-editable")[0]
-            accion = ActionChains(driver)
-            accion.click(campo_pregunta).send_keys(datos_pregunta['pregunta']).perform()
+            # Fill question field
+            question_field = driver.find_elements(By.CLASS_NAME, "note-editable")[0]
+            action = ActionChains(driver)
+            action.click(question_field).send_keys(question_data['question']).perform()
 
-            # Seleccionar el tipo de pregunta según la cantidad de opciones correctas
-            # Si solo hay una opción correcta es "radio" (valor 3 en el menú de la página)
-            # Si tiene múltiples opciones correctas es "checkbox" (valor 1 en el menú de la página)
-            tipo = "3" if len(datos_pregunta['respuesta_correcta']) == 1 else "1"
-            seleccionar_opcion('type_id', tipo)
+            # Select type of question by quantity of answers
+            # Only one correct answer is a "radio" question (value 3 in html menu)
+            # Multiple correct answers is a "checkbox" (value 1 in html menu)
+            type = "3" if len(question_data['correct_answer']) == 1 else "1"
+            select_option('type_id', type)
 
-            # Seleccionar el tópico (siempre el tercero en el menú de la página)
-            seleccionar_opcion('topic_id', '3')
+            # Select topic (not relevant, always the same. In this case the third one)
+            select_option('topic_id', '3')
 
-            # Ingresar puntos de la pregunta (siempre será 10)
-            puntos = driver.find_elements(By.ID, "points")
-            puntos[1].send_keys("10")
+            # Fill question points (always 10)
+            points = driver.find_elements(By.ID, "points")
+            points[1].send_keys("10")
 
-            # Seleccionar la cantidad de respuestas/opciones
-            cantidad_respuestas = str(len(datos_pregunta['opciones']))
-            seleccionar_opcion('cant_questions', cantidad_respuestas)
+            # Select quantity of answers
+            answers_count = str(len(question_data['answers']))
+            select_option('cant_questions', answers_count)
 
-            # Ingresar respuestas/opciones y marcar la(s) correcta(s)
-            for i, opcion in enumerate(datos_pregunta['opciones'], start=1):
+            # Upload answers and check the correct/s one/s box
+            for i, answer in enumerate(question_data['answers'], start=1):
                 WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.NAME, f"answer[{i}]text")))
-                driver.find_element(By.NAME, f"answer[{i}]text").send_keys(opcion)
-                # Chequear si la respuesta ingresada es correcta
-                if i in datos_pregunta['respuesta_correcta']:
+                driver.find_element(By.NAME, f"answer[{i}]text").send_keys(answer)
+                # Check if current question is correct
+                if i in question_data['correct_answer']:
                     driver.find_element(By.NAME, f"is_correct[{i}]correct").click()
             
-            # Ingresar explicación si existe
-            if 'explicacion' in datos_pregunta:
+            # Fill explanation field, if existing
+            if 'explanation' in question_data:
                 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "explicacion")))
-                driver.find_element(By.ID, "explicacion").send_keys(datos_pregunta['explicacion'])
+                driver.find_element(By.ID, "explicacion").send_keys(question_data['explanation'])
 
-            # Ingresar número en el campo objetivos (siempre será 1)
+            # Fill objectives field (always 1)
             driver.find_element(By.ID, "objetivos").send_keys("1")
 
-            # Guardar la pregunta
+            # Save the question
             WebDriverWait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-            boton_guardar = driver.find_element(By.XPATH, "//button[contains(text(), 'Guardar')]")
-            driver.execute_script("arguments[0].click();", boton_guardar)
+            save_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Guardar')]")
+            driver.execute_script("arguments[0].click();", save_button)
 
-            print(f"Pregunta {numero_pregunta} cargada con éxito.")
+            print(f"Pregunta {question_number} cargada con éxito.")
 
-            # Clickear en nueva pregunta para que se habiliten los campos de la nueva pregunta a cargar
+            # Click on new question checkbox to leave all fields shown and ready for next question
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "new_question"))).click()
 
         except Exception as e:
-            print(f"Error al procesar pregunta {numero_pregunta}: {e}")
+            print(f"Error al procesar pregunta {question_number}: {e}")
             input()
 
-    # Abrir el archivo y procesar cada línea
-    with open(f"{examen}.txt", 'r', encoding='utf-8') as archivo:
-        datos_pregunta = {}
-        opciones = []
-        datos_pregunta['respuesta_correcta'] = []
-        for linea in archivo:
-            linea = linea.strip()
-            # Chequear si la línea es una pregunta (Formato: Pregunta 1: ...). 
-            # Se inicia la recopilación de datos
-            if linea.startswith('Pregunta'):
-                numero_pregunta += 1
-                datos_pregunta = {}
-                opciones = []
-                datos_pregunta['respuesta_correcta'] = []
-                # Extraer pregunta
-                datos_pregunta['pregunta'] = linea.split(":", 1)[1].strip()
+    # Open file and process each line
+    with open(f"{exam}.txt", 'r', encoding='utf-8') as file:
+        question_data = {}
+        answers = []
+        question_data['correct_answer'] = []
+        for line in file:
+            line = line.strip()
+            # Check if line is a question (Format: Pregunta 1: ...). 
+            # Beginning of data extraction
+            if line.startswith('Pregunta'):
+                question_number += 1
+                question_data = {}
+                answers = []
+                question_data['correct_answer'] = []
+                # Extract question
+                question_data['question'] = line.split(":", 1)[1].strip()
             
-            # Chequear si la línea es una opción (Formato: A) B) C) etc.), evitando la opción correcta para no tenerla duplicada
-            # Detectar si es una respuesta correcta o incorrecta y eliminar ese texto
-            # Agregar la línea a la lista de opciones de la pregunta
+            # Check if line is an answer (Format: A) B) C) etc.), ignoring correct answer to avoid duplicate answers
+            # Check if answer is correct or incorrect, and delete that information
+            # Add answer to answers list
 
-            elif ')' in linea and len(linea.split(')')[0]) == 1 and not linea.startswith('Opción correcta:'):
-                _, opcion_texto = linea.split(')', 1)
-                if "(Respuesta correcta)" in opcion_texto:
-                    datos_pregunta['opcion_correcta'] = len(opciones) + 1
-                    opcion_texto = opcion_texto.replace("(Respuesta correcta)", "").strip()
-                elif "(Respuesta incorrecta)" in opcion_texto:
-                    opcion_texto = opcion_texto.replace("(Respuesta incorrecta)", "").strip()
-                opciones.append(opcion_texto.strip())
+            elif ')' in line and len(line.split(')')[0]) == 1 and not line.startswith('Opción correcta:'):
+                _, clean_answer = line.split(')', 1)
+                if "(Respuesta correcta)" in clean_answer:
+                    question_data['correct_answer'] = len(answers) + 1
+                    clean_answer = clean_answer.replace("(Respuesta correcta)", "").strip()
+                elif "(Respuesta incorrecta)" in clean_answer:
+                    clean_answer = clean_answer.replace("(Respuesta incorrecta)", "").strip()
+                answers.append(clean_answer.strip())
             
-            # Chequear si la línea es una opción (Formato: 1. 2. 3. etc.), evitando la opción correcta para no tenerla duplicada
-            # Detectar si es una respuesta correcta o incorrecta y eliminar ese texto
-            # Agregar la línea a la lista de opciones de la pregunta
-            elif '.' in linea and linea.strip().split('.')[0].isdigit():
-                _, opcion_texto = linea.split('.', 1)
-                if "(Respuesta correcta)" in opcion_texto:
-                    datos_pregunta['respuesta_correcta'].append(len(opciones) + 1)
-                    opcion_texto = opcion_texto.replace("(Respuesta correcta)", "").strip()
-                elif "(Respuesta incorrecta)" in opcion_texto:
-                    opcion_texto = opcion_texto.replace("(Respuesta incorrecta)", "").strip()
-                opciones.append(opcion_texto.strip())
+            # Check if line is an answer (Format: 1. 2. 3. etc.), ignoring correct answer to avoid duplicate answers
+            # Check if answer is correct or incorrect, and delete that information
+            # Add answer to answers list
+            elif '.' in line and line.strip().split('.')[0].isdigit():
+                _, clean_answer = line.split('.', 1)
+                if "(Respuesta correcta)" in clean_answer:
+                    question_data['correct_answer'].append(len(answers) + 1)
+                    clean_answer = clean_answer.replace("(Respuesta correcta)", "").strip()
+                elif "(Respuesta incorrecta)" in clean_answer:
+                    clean_answer = clean_answer.replace("(Respuesta incorrecta)", "").strip()
+                answers.append(clean_answer.strip())
         
-            # Chequear si la línea es una opción correcta (puede haber más de una)
-            # (Formato de la línea con opción correcta: Opción correcta: A)...)
-            elif linea.startswith('Opción correcta:'):
+            # Check if line is a correct answer (can be multiple correct answers) with format (Opción correcta: A) B) C) etc. or 1. 2. 3. etc.)
+            elif line.startswith('Opción correcta:'):
                 try:
-                    letra_respuesta = linea.split(':')[1].split(")")[0].lower().strip()
-                    indice_correcto = 'abcdefgh'.index(letra_respuesta) + 1
-                    datos_pregunta['respuesta_correcta'].append(indice_correcto)
+                    correct_answer_letter = line.split(':')[1].split(")")[0].lower().strip()
+                    correct_answer_index = 'abcdefgh'.index(correct_answer_letter) + 1
+                    question_data['correct_answer'].append(correct_answer_index)
                 except:
-                    num_respuesta = linea.split(':')[1].split(")")[0].strip()
-                    datos_pregunta['respuesta_correcta'].append(int(num_respuesta))
+                    correct_answer_number = line.split(':')[1].split(")")[0].strip()
+                    question_data['correct_answer'].append(int(correct_answer_number))
             
-            # Chequear si la línea es una explicación (Formato: Explicación: ...)
-            elif linea.startswith('Explicación:'):
-                datos_pregunta['explicacion'] = linea.split(':', 1)[1].strip()
+            # Check if line is an explanation (Format: Explicación: ...)
+            elif line.startswith('Explicación:'):
+                question_data['explanation'] = line.split(':', 1)[1].strip()
             
-            # Fin de la pregunta, agregar la lista opciones al diccionario de la pregunta y llamar a la función que cargará todos los datos en la página
-            elif linea.startswith('!'):
-                datos_pregunta['opciones'] = opciones
-                cargar_pregunta(datos_pregunta)
+            # End of current question, update question data with the answers and call to upload question function 
+            elif line.startswith('!'):
+                question_data['answers'] = answers
+                upload_question(question_data)
 
-# Inicio del programa, del navegador y acceso a la URL
-url = input("Hola Carito :)\nPegá acá el link donde tenés que cargar las preguntas: ")
-examen = input("Ahora escribí tal cual el nombre del archivo (sin el '.txt') que tiene el examen: ")
+# Program start and request of URL and file with questions
+url = input("Hola\nPegá acá el link donde tenés que cargar las preguntas: ")
+exam = input("Ahora escribí tal cual el nombre del archivo (sin el '.txt') que tiene el examen: ")
 
-while examen:
+while exam:
     try:
-        chequeo_archivo = open(f"{examen}.txt", "r")
-        chequeo_archivo.close()
+        file_check = open(f"{exam}.txt", "r")
+        file_check.close()
         break
     except:
-        examen = input("No encuentro el archivo, fijate que esté guardado en la misma carpeta que este programa y que esté en formato '.txt'\nPoné de nuevo el nombre (corroborá que esté escrito tal cual, y sin el '.txt' al final): ")
+        exam = input("No encuentro el archivo, fijate que esté guardado en la misma carpeta que este programa y que esté en formato '.txt'\nPoné de nuevo el nombre (corroborá que esté escrito tal cual, y sin el '.txt' al final): ")
 
 try:
-    driver = iniciar_navegador()
+    driver = start_explorer()
     driver.get(url)
     driver.maximize_window()
 
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "username"))).send_keys("carolina.liendo@educacionit.com")
-    driver.find_element(By.ID, "password").send_keys("42912C5")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "username"))).send_keys("# USER LOGIN CREDENTIALS")
+    driver.find_element(By.ID, "password").send_keys("# USER LOGIN CREDENTIALS")
     driver.find_element(By.ID, "loginButton").click()
 
     driver.get(url)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "new_question"))).click()
 
-    procesar_preguntas()
+    process_questions()
 
 except Exception as e:
     print(f"Se ha producido un error: {e}")
 
 finally:
-    input("Listo Carito, se cargaron todas las preguntas :)\nApretá ENTER para cerrar el programa.\n")
+    input("Se cargaron todas las preguntas\nApretá ENTER para cerrar el programa.\n")
     driver.quit()
